d635641f9f47346a1b57b5cc4dd70d4fc7a5854f
Danish Khan <askdnk2523@gmail.com>
2025-03-07 09:23:20 +0530

    Add files via upload

---
+++ entertainment/entertainment.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Entertainment | WEB'S 360</title>
+    <link rel="stylesheet" href="entertainment.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+    <script defer src="entertainment.js"></script>
+</head>
+<body>
+    <section id="intro">
+        <h1>Welcome to WEB'S 360 Entertainment</h1>
+        <p>Experience the best entertainment across Movies, Songs, Games, Live Events, and Blogs. Explore now!</p>
+    </section>
+    <p class="par"> Select What Type Of Entertainment Do You Want</p>
+    <header>
+        <nav>
+            <ul class="nav-links">
+                <li><a href="https://hindilinks4u.taxi/" target="_blank">Movies</a></li>
+                <li><a href="https://open.spotify.com/" target="_blank">Songs</a></li>
+                <li><a href="https://playhop.com/?" target="_blank">Games</a></li>
+                <li><a href="https://www.youtube.com/live/IXyjKzWB6D4?si=2ByaSZDUxEVKYfX8" target="_blank">Live Events</a></li>
+                <li><a href="https://everyday-reading.com/blog-reading/" target="_blank">Blogs</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+
+    
+   
+    <footer class="footer">
+  <div class="footer-container">
+    <div class="footer-section">
+      <h4>ABOUT</h4>
+      <a href="\contactus.html">Contact Us</a>
+      <a href="/aboutus.html">About Us</a>
+    </div>
+
+    <div class="footer-section">
+      <h4>Entertainment Partners</h4>
+      <a href="https://hindilinks4u.taxi/" target="_blank">Hindi Links For You</a>
+      <a href="https://open.spotify.com/" target="_blank">Spotify</a>
+      <a href="https://playhop.com/?" target="_blank">Play Hop</a>
+      <a href="https://www.youtube.com/" target="_blank">YouTube</a>
+      <a href="https://everyday-reading.com/blog-reading/" target="_blank">Everyday Blogs</a>
+    </div>
+
+
+    <div class="footer-section contact-info">
+      <h4>Mail Us:</h4>
+      <p>WEB'S 360 Private Limited<br>
+        07, Sangam Park<br>
+        Behind S.G.R.G. Shinde Collage <br>
+        Bawachi Road Paranda<br>
+        Osmanabad(Dharashiv), 413502,<br>
+        Maharashtra, India
+      </p>
+  </div>
+  </div>
+</footer>
+</body>
+</html>
