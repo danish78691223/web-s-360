bffc571263274ab42514238e04ed324f253942cb
Danish Khan <askdnk2523@gmail.com>
2025-04-04 19:29:56 +0530

    Delete admin directory

--- admin/assets/js/dashboard.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("toggle-btn").addEventListener("click", function () {
-    let sidebar = document.querySelector(".sidebar");
-    if (sidebar.style.width === "260px") {
-        sidebar.style.width = "0";
-    } else {
-        sidebar.style.width = "260px";
-    }
-});
